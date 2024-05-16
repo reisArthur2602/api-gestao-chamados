351734e80ea6b41f3892c641b3cbe83b617d8329
arthur <arthurdesouzareis222@gmail.com>
2024-05-16 13:47:03 -0300

    feat: criar arquivo de rotas

---
+++ src/routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+export const router = Router();
+
+router.get('/', (req, res) => console.log('Hello Word'));
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
+import 'dotenv/config';
+
 import express from 'express';
 import cors from 'cors';
-
+import { router } from './routes';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 app.listen(process.env.PORT, () =>
   console.log(`🚀 Server rodando na porta ${process.env.PORT}`)
