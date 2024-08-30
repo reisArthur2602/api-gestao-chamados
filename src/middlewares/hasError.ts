9cb9d7bd074d3145c759422595eed5e404e4eb6e
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-30 16:02:16 -0300

    chore : add zod error message to has Error middleware

--- src/middlewares/hasError.ts
+++ src/middlewares/hasError.ts
@@ -1,16 +1,22 @@
 import { NextFunction, Response, Request } from 'express';
 
 import { ZodError } from 'zod';
-import { AplicationError } from '../helpers/error';
+import { AplicationError, BadRequestError } from '../helpers/error';
+import { StatusCodes } from 'http-status-codes';
 
 export const hasError = (
     error: Error & Partial<AplicationError>,
-    req: Request,
-    res: Response,
+    reques: Request,
+    response: Response,
     next: NextFunction
 ) => {
+    if (error instanceof ZodError)
+        return response
+            .status(StatusCodes.BAD_REQUEST)
+            .json({ message: 'Preencha dos dados corretamente' });
+
     const statusCode = error.statusCode ?? 500;
     const message = error.message ?? 'Internal Server Error';
     console.error({ message });
-    return res.status(statusCode).json({ message });
+    return response.status(statusCode).json({ message });
 };
